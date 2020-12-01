41b5bff2fbe4c449ada167ca86c410f362c56a40
tomertv2 <tomertv2@gmail.com>
2020-12-01 15:32:33 +0200

    Initialize react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stronghold Scraper</h1>
+      <div className='content'>
+        <div className='posts'>
+          here we have a content about posts div
+        </div>
+        <div className='analytics'>
+        here we have a content about analytics div
+        </div>
+      </div>
     </div>
   );
 }
